be20b218a55d05505a45fe7391432c18bb62b67a
PravallikaParvatini <PravallikaParvatini@missionrnd.com>
2016-03-27 13:15:34 +0530

    Submit Commit

--- src/sortedArraysCommonElements.cpp
+++ src/sortedArraysCommonElements.cpp
@@ -25,5 +25,34 @@ struct transaction {
 };
 
 struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	int y_a, y_b, m_a, m_b, d_a, d_b,i=0,j=0,k=0;
+	struct transaction *final=NULL;
+	if (A == NULL || ALen <= 0 | B == NULL || BLen <= 0)
+		return NULL;
+	while (i < ALen)
+	{
+		y_a = (A[i].date[6] - '0') * 1000 + (A[i].date[7] - '0') * 100 + (A[i].date[8] - '0') * 10 + (A[i].date[9] - '0');
+		m_a = (A[i].date[3] - '0') * 10 + (A[i].date[4] - '0');
+		d_a = (A[i].date[0] - '0') * 10 + (A[i].date[1] - '0');
+		while (j < BLen)
+		{
+			y_b = (B[i].date[6] - '0') * 1000 + (B[i].date[7] - '0') * 100 + (B[i].date[8] - '0') * 10 + (B[i].date[9] - '0');
+			m_b = (B[i].date[3] - '0') * 10 + (B[i].date[4] - '0');
+			d_b = (B[i].date[0] - '0') * 10 + (B[i].date[1] - '0');
+			if (y_a == y_b&&m_a == m_b&&d_a == d_b)
+			{
+				final = (struct transaction*)realloc(final, (k + 1)*sizeof(struct transaction));
+				final[k] = B[j];
+				j++;
+				k++;
+			}
+			else
+				j++;
+		}
+		i++;
+	}
+	if (final == NULL)
+		return NULL;
+	else
+		return final;
 }
